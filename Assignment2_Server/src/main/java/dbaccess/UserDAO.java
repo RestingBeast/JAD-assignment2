a23a5a0a7e65e8e61b97aa67e45b9d71cd66c564
HeinHtetAung5142 <hhaung5142@gmail.com>
2022-08-07 17:30:29 +0800

    UserService complete.
    UserDAO & UserService completed. Servlets not modified.

--- Assignment2_Server/src/main/java/dbaccess/UserDAO.java
+++ Assignment2_Server/src/main/java/dbaccess/UserDAO.java
@@ -1,8 +1,56 @@
 package dbaccess;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class UserDAO {
+	public ArrayList<User> listAllUsers() {
+		
+		ArrayList<User> users = new ArrayList<User>();
+		User u = null;
+		Connection conn = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			String connURL = "jdbc:mysql://localhost:3306/assignment2?user=root&password=Root1234-&serverTimezone=UTC";
+			
+			conn = DriverManager.getConnection(connURL);
+			
+			String sqlStr = "SELECT * FROM user";
+			
+			PreparedStatement ps = conn.prepareStatement(sqlStr);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				u = new User();
+				
+				u.setUserId(rs.getInt("userid"));
+				u.setUsername(rs.getString("username"));
+				u.setPassword(rs.getString("password"));
+				u.setEmail(rs.getString("email"));
+				u.setRole(rs.getString("role"));
+				u.setProfile_Pic_Url("profile_pic_url");
+				
+				users.add(u);
+			}
+		} catch (Exception e) {
+			System.out.print("Exception: " + e);
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			}
+			catch(Exception e) {
+				System.out.println("Closing error :" + e);
+			}
+		}
+		
+		return users;
+	}
+
 	public User findById(int userID) {
 		User u = null;
 		Connection conn = null;
@@ -46,24 +94,113 @@ public class UserDAO {
 		
 		return u;
 	}
-	
-	public static String getUserNameById(int i) throws Exception {
-		String name = "";
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		
-		String connURL = "jdbc:mysql://localhost:3306/assignment1?user=root&password=Root1234-&serverTimezone=UTC";
 
-        Connection conn = DriverManager.getConnection(connURL); 
-		
-		String sqlStr = "SELECT * FROM user WHERE userid = ?";
-		
-		PreparedStatement ps = conn.prepareStatement(sqlStr);
-		ps.setInt(1, i);
-
-		ResultSet rs = ps.executeQuery();
-		if (rs.next()) {
-			name = rs.getString("username");
+	public int createUser(String name, String email, String pwd, String role, String pfp) {
+		Connection conn = null;
+		int rowsAffected = -1;
+		try {
+			if (role != null) {
+				role = "Admin";
+			} else {
+				role = "Member";
+			}
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			String connURL = "jdbc:mysql://localhost:3306/assignment1?user=root&password=Root1234-&serverTimezone=UTC";
+			
+			conn = DriverManager.getConnection(connURL);
+			
+			String sqlStr = "INSERT INTO user (username, email, password, role, profile_pic_url) VALUES (?, ?, ?, ?, ?)";
+			
+			PreparedStatement ps = conn.prepareStatement(sqlStr);
+			ps.setString(1, name);
+			ps.setString(2, email);
+			ps.setString(3, pwd);
+			ps.setString(4, role);
+			ps.setString(5, pfp);
+			
+			rowsAffected = ps.executeUpdate();
 		}
-		return name;
+		catch (Exception e) {
+			System.out.println("Exception :" + e);
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				System.out.println("Closing error :" + e);
+			}
+		}
+		return rowsAffected;
+	}
+	
+	public int deleteUser(int uid) {
+		Connection conn = null;
+		int rowsAffected = -1;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			String connURL = "jdbc:mysql://localhost:3306/assignment1?user=root&password=Root1234-&serverTimezone=UTC";
+			
+			conn = DriverManager.getConnection(connURL);
+			
+			String sqlStr = "DELETE FROM user WHERE userid = ?";
+			
+			PreparedStatement ps = conn.prepareStatement(sqlStr);
+			ps.setInt(1, uid);
+			
+			rowsAffected = ps.executeUpdate();
+		}
+		catch (Exception e) {
+			System.out.println("Exception :" + e);
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				System.out.println("Closing error :" + e);
+			}
+		}
+		return rowsAffected;
+	}
+	
+	public int updateUser(int uid, String name, String email, String pwd, String role, String pfp) {
+		Connection conn = null;
+		int rowsAffected = -1;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			String connURL = "jdbc:mysql://localhost:3306/assignment1?user=root&password=Root1234-&serverTimezone=UTC";
+			
+			conn = DriverManager.getConnection(connURL);
+			
+			String sqlStr = "UPDATE user SET username = ?, email = ?, password = ?, role = ?, "
+					+ "profile_pic_url = ? WHERE userid = ?";
+			
+			PreparedStatement ps = conn.prepareStatement(sqlStr);
+			ps.setString(1, name);
+			ps.setString(2, email);
+			ps.setString(3, pwd);
+			ps.setString(4, role);
+			ps.setString(5, pfp);
+			ps.setInt(6, uid);
+			
+			rowsAffected = ps.executeUpdate();
+		}
+		catch (Exception e) {
+			System.out.println("Exception :" + e);
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				System.out.println("Closing error :" + e);
+			}
+		}
+		return rowsAffected;
 	}
 }
--- Assignment2_Server/src/main/java/webservices/UserService.java
+++ Assignment2_Server/src/main/java/webservices/UserService.java
@@ -1,19 +1,46 @@
 package webservices;
 
+import java.io.StringReader;
 import java.util.ArrayList;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.stream.JsonParsingException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import dbaccess.TourDAO;
 import dbaccess.User;
 import dbaccess.UserDAO;
 
 @Path("/UserService")
 public class UserService {
+	@GET
+	@Path("/listAllUsers")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response listallUsers() {
+		ArrayList<User> userArr = new ArrayList<>();
+		try {
+			UserDAO db = new UserDAO();
+			userArr = db.listAllUsers();
+		} catch (Exception e) {
+			System.out.println("Error :" + e);
+		}
+		
+		return Response
+				.status(Response.Status.OK)
+				.entity(userArr)
+				.build();
+	}
+	
 	@GET
 	@Path("/getUser")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -29,6 +56,139 @@ public class UserService {
 		return Response
 				.status(Response.Status.OK)
 				.entity(user)
+				.build();
+	}
+	
+	@PUT
+	@Path("/createUser")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response createUser(String inputData) {
+		int rowsAffected = -1;
+		String jsonOutput = "";
+		
+		try {
+			JsonReader jsonReader = Json.createReader(new StringReader(inputData));
+			
+			JsonObject inputJSONObj = jsonReader.readObject();
+			
+			String name = inputJSONObj.getString("name");
+			String email = inputJSONObj.getString("email");
+			String password = inputJSONObj.getString("password");
+			String pfp = inputJSONObj.getString("profile_pic_url");
+			
+			// data validation (To check if data is feasible or not)
+			// can include checks for name/email availability but no time to do now.
+			
+			UserDAO db = new UserDAO();
+			
+			rowsAffected = db.createUser(name, email, null ,password, pfp);
+			jsonOutput = "{\"rows affected\" : \"" + rowsAffected + "\"" + "}";
+			
+		} catch (JsonParsingException e) {
+			String someError = "{" + "\"error\" : \"bad input data\"" + "}";
+			
+			return Response
+					.status(Response.Status.BAD_REQUEST)
+					.entity(someError)
+					.build();
+			
+		} catch (NullPointerException e) {
+			String someError = "{" + "\"error\" : \"bad input date\"," + "\"details\" : \"" + 
+								e.toString().replace("java.lang.NullPointerException: ", "") + "}";
+			
+			return Response
+					.status(Response.Status.BAD_REQUEST)
+					.entity(someError)
+					.build();
+			
+		} catch (Exception e) {
+			return Response
+					.status(Response.Status.BAD_REQUEST)
+					.entity("{\"error\" : \"" + e.toString() + "\"" + "}")
+					.build();
+		}
+		
+		return Response
+				.status(Response.Status.OK)
+				.entity(jsonOutput)
+				.build();
+	}
+	
+	@DELETE
+	@Path("/deleteUser")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response deleteTour(@QueryParam("userid") String useridStr) {
+		int userid = Integer.parseInt(useridStr);
+		int rowsAffected = -1;
+		String jsonOutput = "";
+		try {
+			UserDAO db = new UserDAO();
+			rowsAffected = db.deleteUser(userid);
+			jsonOutput = "{" +
+					"\"affectedRows\": " + rowsAffected +
+			"}";
+		} catch(Exception e) {
+			System.out.println("Exception: " + e);
+		}
+		return Response
+				.status(Response.Status.OK)
+				.entity(jsonOutput)
+				.build();
+	}
+	
+	@PUT
+	@Path("/updateUser")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response updateTour(@QueryParam("userid") String useridStr, String inputData) {
+		int userid = Integer.parseInt(useridStr);
+		int rowsAffected = -1;
+		String jsonOutput="";
+		try {
+			JsonReader jsonReader = Json.createReader(new StringReader(inputData));
+			
+			JsonObject inputJSONObj = jsonReader.readObject();
+			
+			String name = inputJSONObj.getString("name");
+			String email = inputJSONObj.getString("email");
+			String password = inputJSONObj.getString("password");
+			String role = inputJSONObj.getString("role");
+			String pfp = inputJSONObj.getString("profile_pic_url");			
+			
+			UserDAO db = new UserDAO();
+			rowsAffected = db.updateUser(userid, name, email, password, role, pfp);
+			jsonOutput = "{" +
+					"\"affectedRows\": " + rowsAffected +
+			"}";
+		}catch (NullPointerException e) {
+			String someError = "{" +
+					"\"error\": \"bad input data\"\n" +
+					"\"details\": \"" + e.toString().replace("java.lang.NullPointerException: ", "") + "\"" +
+			"}";
+			return Response
+					.status(Response.Status.BAD_REQUEST)
+					.entity(someError)
+					.build();
+		}
+		catch (JsonParsingException e) {
+			String someError = "{" +
+					"\"error\": \"bad input data\"" +
+			"}";
+			return Response
+					.status(Response.Status.BAD_REQUEST)
+					.entity(someError)
+					.build();
+		}
+		catch (Exception e) {
+			return Response
+					.status(Response.Status.BAD_REQUEST)
+					.entity("{\n\"error\": \"" + e.toString() + "\"\n}")
+					.build();
+		}
+		return Response
+				.status(Response.Status.CREATED)
+				.entity(jsonOutput)
 				.build();
 	}
 }
