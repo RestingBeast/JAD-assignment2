dc1d167a583dc343c26997f3315ca3f4e46e74ff
HeinHtetAung5142 <hhaung5142@gmail.com>
2022-08-07 18:53:08 +0800

    Maven setup
    Project converted to maven for Paypal integration.

--- Assignment2_Client/src/main/java/classes/ReviewUtils.java
+++ Assignment2_Client/src/main/java/classes/ReviewUtils.java
@@ -7,16 +7,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-//import javax.servlet.*;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.ws.rs.client.Client;
-//import javax.ws.rs.client.ClientBuilder;
-//import javax.ws.rs.client.Invocation;
-//import javax.ws.rs.client.WebTarget;
-//import javax.ws.rs.core.GenericType;
-//import javax.ws.rs.core.MediaType;
-//import javax.ws.rs.core.Response;
 
 public class ReviewUtils {
 	
